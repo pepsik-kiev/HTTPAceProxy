2b6fe454a63430876a6343fa1ba5b4a79ecc9cce
pepsik-kiev <pepsik@ukr.net>
2019-01-14 01:07:59 +0200

    ** Fix 0B size in info pannel **

--- http/js/index.js
+++ http/js/index.js
@@ -91,6 +91,6 @@ function renderPage(data) {
 }
 
 function bytes2human(size) {
-    var i = Math.floor( Math.log(size) / Math.log(1024) );
+    var i = size == 0 ? 0 : Math.floor( Math.log(size) / Math.log(1024) );
     return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
 }
