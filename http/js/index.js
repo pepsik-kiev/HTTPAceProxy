84dffeaa218244cbd4677eb2de54121861fca9a2
Joy <videomiv@gmail.com>
2019-01-18 18:49:41 +0200

    first test variant

--- http/js/index.js
+++ http/js/index.js
@@ -64,8 +64,6 @@ $(document).ready(function() {
         var sys_info = data.sys_info;
         var connection_info = data.connection_info;
         var clients_data = data.clients_data;
-        var clients_content = "";
-
 
         // Header System Info
         $inf_os.text(sys_info.os_platform);
@@ -78,7 +76,7 @@ $(document).ready(function() {
         $inf_disk_used.text("used: " + bytes2human(sys_info.disk_info['used']));
         $inf_disk_free.text("free: " + bytes2human(sys_info.disk_info['free']));
 
-        if (sys_info.cpu_freq) {
+        if (sys_info.cpu_freq.current) {
             $inf_cpu_freq.text("freq: " + sys_info.cpu_freq['current'] + " Mhz");
         };
 
@@ -92,7 +90,7 @@ $(document).ready(function() {
 
         // Display Header System Info
         if (!init_header) {
-            if (sys_info.cpu_freq) $inf_cpu_freq.removeClass('d-none');
+            if (sys_info.cpu_freq.current) $inf_cpu_freq.removeClass('d-none');
             if (sys_info.cpu_temp) $inf_temp.removeClass('d-none');
             $('.header .invisible').removeClass('invisible').removeClass('transparent');
             init_header = true;
@@ -100,7 +98,12 @@ $(document).ready(function() {
 
         // Table body
         if (clients_data.length) {
+
             clients_data.forEach(function(item, i, arr) {
+
+                var rowID = createID(item);
+                var $tbody = $('tbody');
+                var $row = $('#' + rowID);
 
                 var statusColorCss = {
                     wait: 'warning',
@@ -111,35 +114,89 @@ $(document).ready(function() {
 
                 var badgeCss = statusColorCss[item.stat['status']] || 'danger';
 
-                var clientInfo = item.clientInfo.vendor ? item.clientInfo.vendor :
-                    '<i class="flag ' + (item.clientInfo.country_code.toLowerCase() || 'n/a') + '"></i>&nbsp;&nbsp;' +
-                    (item.clientInfo.country_name || 'n/a') +', ' + (item.clientInfo.city || 'n/a');
+                if (!$row.length) {
 
-                clients_content += '<tr title="Downloaded: ' + bytes2human(item.stat['downloaded']) + ' Uploaded: ' + bytes2human(item.stat['uploaded']) + '">'+
-                                    '<td><img src="' + item.channelIcon + '"/>&nbsp;&nbsp;' + item.channelName + '</td>' +
-                                    '<td>' + item.clientIP + '</td>'+
-                                    '<td>' + clientInfo + '</td>' +
-                                    '<td class="text-center">' + item.startTime + '</td>' +
-                                    '<td class="text-center">' + item.durationTime + '</td>' +
-                                    '<td class="text-center">' +
-                                        '<div class="digit-speed text-right">'+ item.stat['speed_down'] + '</div>' +
-                                        '<img src="/stat/img/arrow-down.svg"/><img src="/stat/img/arrow-up.svg"/>' +
-                                        '<div class="digit-speed text-left">' + item.stat['speed_up'] + '</div></td>' +
-                                    '<td class="text-center">'+ item.stat['peers'] +
-                                    '<span class="badge badge-pill badge-'+ badgeCss + ' bage-fixsize">' + item.stat['status'] + '</span></td></tr>';
+                    var clientInfo = item.clientInfo.vendor ? item.clientInfo.vendor :
+                        '<i class="flag ' + (item.clientInfo.country_code.toLowerCase() || 'n/a') + '"></i>&nbsp;&nbsp;' +
+                        (item.clientInfo.country_name || 'n/a') +', ' + (item.clientInfo.city || 'n/a');
 
+                    var peers = typeof item.stat['peers'] == "undefined" ? "n/a" :
+                        item.stat['peers'] + '<span class="badge badge-pill badge-'+ badgeCss + ' bage-fixsize">' + item.stat['status'] + '</span>';
+
+                    var speed_down = typeof item.stat['speed_down'] == "undefined" ? "n/a" : item.stat['speed_down'];
+                    var speed_up = typeof item.stat['speed_up'] == "undefined" ? "n/a" : item.stat['speed_up'];
+
+                    var row  =  '<tr id="' + rowID + '">' +
+                                '<td><img src="' + item.channelIcon + '"/>&nbsp;&nbsp;' + item.channelName + '</td>' +
+                                '<td>' + item.clientIP + '</td>'+
+                                '<td>' + clientInfo + '</td>' +
+                                '<td class="text-center">' + item.startTime + '</td>' +
+                                '<td class="text-center duration-time">' + item.durationTime + '</td>' +
+                                '<td class="text-center">' +
+                                    '<div class="digit-speed text-right speed-down">'+ speed_down + '</div>' +
+                                    '<img src="/stat/img/arrow-down.svg"/><img src="/stat/img/arrow-up.svg"/>' +
+                                    '<div class="digit-speed text-left speed-up">' + speed_up + '</div></td>' +
+                                '<td class="text-center peers">'+ peers + '</td></tr>';
+
+                    $tbody.append(row);
+                    $('#' + rowID).data('update', true)
+                        .attr('title', 'Downloaded: ' + bytes2human(item.stat['downloaded']) + ' Uploaded: ' + bytes2human(item.stat['uploaded']));
+
+                } else {
+                    $row.attr('title', 'Downloaded: ' + bytes2human(item.stat['downloaded']) + ' Uploaded: ' + bytes2human(item.stat['uploaded']));
+                    $row.find('.duration-time').text(item.durationTime);
+                    $row.find('.speed-down').text(item.stat['speed_down']);
+                    $row.find('.speed-up').text(item.stat['speed_up']);
+                    $row.find('.peers').html(item.stat['peers'] + '<span class="badge badge-pill badge-'+ badgeCss + ' bage-fixsize">' + item.stat['status'] + '</span>');
+                    $row.data('update', true);
+                }
+
+                $('tbody tr').each(function(index) {
+                    if (!$(this).data('update')) {
+                        $(this).remove();
+                    };
+                });
             });
 
-            $('tbody').html(clients_content);
         } else {
 
             $('tbody').html('');
         }
+
+        $('tbody tr').data('update', false);
     }
 
     function bytes2human(size) {
         var i = size == 0 ? 0 : Math.floor( Math.log(size) / Math.log(1024) );
         return ( size / Math.pow(1024, i) ).toFixed(2) * 1 + ' ' + ['B', 'kB', 'MB', 'GB', 'TB'][i];
     }
+
+    // function Row(client) {
+    //     var tr = document.createElement('tr');
+
+    //     this.td = document.createElement('td');
+    //     this.id = createID(client);
+    //     this.text = "TEST";
+    //     this.updateText = function(text) {
+    //         this.td.innerText = text;
+    //     }
+    // }
+
+    function createID(item) {
+        var client = item.channelName + item.clientIP + item.startTime;
+        var rowId = "rowId" + client.hashCode();
+        return rowId;
+    }
+
+    String.prototype.hashCode = function() {
+        var hash = 0, i, chr;
+        if (this.length === 0) return hash;
+        for (i = 0; i < this.length; i++) {
+            chr   = this.charCodeAt(i);
+            hash  = ((hash << 5) - hash) + chr;
+            hash |= 0; // Convert to 32bit integer
+        }
+        return hash;
+    };
 });
 
