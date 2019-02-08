3ce6befae27cc0b0b178710ce3477f8e179b6777
pepsik-kiev <pepsik@ukr.net>
2019-02-08 21:39:18 +0200

    ** Update channel picons **

--- plugins/config/torrenttelik.py
+++ plugins/config/torrenttelik.py
@@ -1,47 +1,143 @@
+# -*- coding: utf-8 -*-
 '''
-Torrent-telik.com. Configuration file for json-based playlists downloader
-
-Playlist format example:
-{"channels":[
-{"name":"Channel name 1","url":"blablablablablablablablablablablablablab","cat":"Group 1"},
-{"name":"Channel name 2","url":"blablablablablablablablablablablablablab","cat":"Group 2"},
-{"name":"Channel name 3","url":"blablablablablablablablablablablablablab","cat":"Group 3"},
-......
-...
-..
-.
-{"name":"Channel name N","url":"blablablablablablablablablablablablablab","cat":"Group N"}
-]}
-
+Torrent-telik channels picon
 '''
-# Proxy settings.
-# For example you can install tor browser and add in torrc SOCKSPort 9050
-# if you use tor on the same machine with AceProxy -  proxies = { 'https' : 'socks5h://127.0.0.1:9050' }
-# If your http-proxy need authentification - proxies = {https' : 'https://user:password@ip:port'}
-
-proxies = None
-#proxies = {'http' : 'socks5h://192.168.1.1:9100', 'https' : 'socks5h://192.168.1.1:9100'}
-
-# Channels urls
-url = 'http://91.92.66.82/trash/ttv-list/ace.json'
-
-# EPG urls
-tvgurl = 'http://epg.do.am/tv.gz'
-
-# Shift the TV Guide time to the specified number of hours
-tvgshift = 0
-
-# Download playlist every N minutes to keep it fresh
-# 0 = disabled
-updateevery = 120
-
-# Channel playlist template
-# The following values are allowed:
-# name - channel name
-# url - channel URL
-# tvg - channel tvg-name (optional)
-# tvgid - channel tvg-id (optional)
-# group - channel playlist group-title (optional)
-# logo - channel picon file tvg-logo (optional)
-m3uheadertemplate = '#EXTM3U url-tvg="%s" tvg-shift=%d deinterlace=1 m3uautoload=1 cache=1000\n' % (tvgurl, tvgshift)
-m3uchanneltemplate = '#EXTINF:-1 group-title="%(group)s" tvg-name="%(tvg)s" tvg-logo="%(logo)s",%(name)s\n#EXTGRP:%(group)s\n%(url)s\n'
+# Channels logo location
+logobase = 'http://1ttv.org/uploads/'
+# Channels logo/path mapping
+logomap = {
+    u"24Техно": logobase + 'JbUGHLuuZa3WQbjtbzUo0cDZkGnLRK.png',
+    u"365 дней ТВ": logobase + '0IZrVwoxtmjtgnWu5Dj4Hb8FRc8NIX.png',
+#    u"Animal Family HD": logobase + '',
+    u"Animal Planet HD": logobase + '45.png',
+    u"Discovery Science HD": logobase + 'lyo9uHELb4es3DBo5p0ztPw0JnLaEf.png',
+    u"Discovery Россия HD": logobase + 'SmWnYlOvkJn8GzttT2UY0vmo8PYfMg.png',
+    u"Eureka HD": logobase + 'OaWtWXpPyC1adsRP69CUPBnJPrT0lt.png',
+    u"HD-Life": logobase + 'jUteUS0xRGdvLyBVqNjowEUDkOjT0t.png',
+    u"History Россия HD": logobase + 'VFgU260pmiIyPxCzD3f8R7Yc6DXClH.png',
+    u"History2 HD": logobase + 'TaToxTsGJnMk9nvWCBqfZvcJ2GyGUC.png',
+    u"ID Xtra Россия": logobase + 'fq3ovkZeu4R4YVYt1VMZN1NU7mMrH9.png',
+    u"National Geographic": logobase + 'i6STSw6Hg1wWP18yBAOyKoKpSMeKLu.png',
+    u"National Geographic HD": logobase + 'hK1waimMq9eAp0ugM19moSoQvUeve5.png',
+    u"Outdoor channel HD": logobase + 'SuxFoocUmay5G8jtfJaHL1yIbAT5Hr.png',
+#    u"RT Д HD": logobase + '',
+    u"TLC Россия": logobase + 'gT4olUY9nFJbGRCdwd7hHJp1NJ5eJr.png',
+    u"Travel channel HD": logobase + 'zfnAGLCvIu1fx9hfrITAZMoo9HYww4.png',
+    u"Travel+Adventure HD": logobase + 'b1HifWKMyefmDDvaDAJTwNNTaD8LF4.png',
+    u'Viasat History': logobase + 'MWGbB8wJp5Gm4vbPHl0ktohDDjMKdr.png',
+    u"Viasat Nature/History HD": logobase + 'pSP6zxmuO4PU6xa6KRlZ9L8vvVM2Dy.png',
+    u"Авто Плюс": logobase + 'WkRxjy6fJEBJ5NZiaGn2j05eqfFfQq.png',
+    u"Авто24": logobase + 'GZwKgvkC0vfYquFAn1dKhppNxdDit9.png',
+    u"Доктор": logobase + 'FznqTpJ71LS3RLESzMOHhNDxJMwBE8.png',
+    u"Еда HD": logobase + 'ojUD1jhpv7HBOLmubpEBOsANkpYNtk.png',
+    u"Живая планета": logobase + 'xgKSMwqBdEyXnbVgb8LtNXSMiaPcOx.png',
+    u"Живая природа HD": logobase + 'L9qmq6UIprbg4VBQA46tqRjQ22thsp.png',
+    u"Кухня ТВ": logobase + 'G0WbVMphlP9oJ6KvHRfx0xDfhrF9Re.png',
+    u"Оружие": logobase + 'CyDUCmYXK8WS2kXCX5kiAOFejnlwoP.png',
+    u"Охота и рыбалка": logobase + '5l2P20J6ebTh0ptOr27Hh704niP3nU.png',
+    u"Планета HD": logobase + '1QjirpCLi3q9qPu1CTvEvCB0BfINeo.png',
+    u"Страна": logobase + 'ysrRW9deFkccNGlhtT0Sww5Yt8IpY1.png',
+    u"Телепутешествия HD": logobase + 'fz4bqwLySJAQkUN7l2EPKNqyvilfRD.png',
+    u"1+1": logobase + 'omm2Xc8xSVIT6Od6ca4QqMrEXw3jaK.png',
+    u"2+2": logobase + 'XHXBC3ghvhh100BNXylSgJLx5FVQgD.png',
+    u"2х2": logobase + 'hTpJUV15GSTxZ5kJQGLcn42kCzKyEH.png',
+    u"3+": logobase + 'AHb8YFuSBRZbcILUWjCu6n6PtyH9Qh.png',
+    u"5 канал (Россия)": logobase + 'nIUDYY41OO4Xo0ntGpGv2rfpOR5ngt.png',
+#    u"9 канал (Израиль)": logobase + '',
+    u"ATV HD": logobase + 'yx7GzFSH3BlxYYa0Nuq4eiVRqxZSa5.png',
+    u"Armenia TV": logobase + 'M4UAMT6cEyL3zuim9fzy38vZRkIfKq.png',
+    u"ICTV": logobase + 'YuNtYxhj9vqgTU9kVuz9imhqviY4PZ.png',
+#    u"LNK": logobase + '',
+    u"LRT": logobase + 'iT3NgoFBu7R4wQ0yDQ9WQsMMJQfIh0.png',
+    u"RTVi": logobase + '8grb3H6aQ5XVf9Vouy01qGvirehQvW.png',
+    u"Shant HD": logobase + '2CmRmYOEq9RADmLInEL28WVzdgvYnu.png',
+#    u"Астана ТВ": logobase + '',
+    u"Беларусь 1": logobase + 'S32pk5GPcXbTyyCllIjfpqPPkBAy4Q.png',
+    u'Дом кино': logobase + 'jlC78Fy13KWjQUN6l3FtbsRLZDvc0x.png',
+    u"Интер": logobase + 'frjd3J6O1RJ6ki9JooQOmLKCb4VuSZ.png',
+    u"КТК": logobase + 'BhI4KURCZ3fAgCQcwQWn7vRCJQblH3.png',
+#    u"Культура": logobase + '',
+    u"МирТВ": logobase + 'Oq6h2IicTagHENQu1mFkjLk5rChMnr.png',
+    u'НТВ HD': logobase + 'zdJ3ye6d3UWl5a56zm6LjqYH6ziSOs.png',
+    u"НТВ (+2)": logobase + 'B5GA1cfgmn8EsxrdwfNUIrEbdqarXf.png',
+    u'ОТР': logobase + 'CqxKorK72v3ULbWkB3ZOhdte0duYZa.png',
+    u"Первый канал (+2)": logobase + 'GGe1ttMtczS0mgzMCegYji57J3PIaJ.png',
+    u"Первый канал HD": logobase + 'VxAFWzh1y88c8Aqa17TsxD2IO5pqoi.png',
+    u"Первый канал HD (+4)": logobase + 'VxAFWzh1y88c8Aqa17TsxD2IO5pqoi.png',
+    u"Пятница": logobase + '0fafj6PSIWdqtBdgwYTl9M06SDU2wA.png',
+    u"РЕН ТВ": logobase + 'LJvkfB2kYaDzij1Y13Fy6syUCkP5Y6.png',
+    u"РТР-Планета": logobase + 'jgQeBwyqQI1Qh1r2aeOK5NiyPtcjTd.png',
+    u"Россия 1 (+2)": logobase + 'UUrfoqi6NQcc9gRLnCc8ODZJ2T3ShE.png',
+    u'Россия HD': logobase + 'ghvqmVpPWqn9x6POAm9UJBvXFzTrqN.png',
+    u"Сарафан": logobase + 'LsYzwEOUspoxkY2hrTSy9zKqvpWlY8.png',
+    u'СТБ': logobase + 'saZlIDrdaXWoiQa8sfZp2bEAeH0kXk.png',
+    u"ТВ3 Россия": logobase + '07bjT0brQLyETHscCfGFtvsdaRiUdH.png',
+    u'ТВЦ': logobase + 'QEpQTskZ9hcfI0rgD8osHVYSv58pde.png',
+    u"ТНТ (+2)": logobase + 'Vtt1KKIpLY4LTQGnV03sdBYyX3hyWR.png',
+    u"ТНТ HD": logobase + 'iD2Gi2nqvSSZnrtPeaqAp6M3NpD50v.png',
+    u"ТНТ4": logobase + 'yTclqOAW0EWwhw9vt0spVSUcS70ZR0.png',
+    u"Brazzers TV": logobase + 'VPCmFsMfpnB0pg9VBzvUG5TkRcHTsR.png',
+#    u"Dorcel HD": logobase + '',
+    u"Playboy TV": logobase + 'lIWBxmt5GDl9tg4KsQNtA0CuZWdOHH.png',
+    u"НЮ АРТ": logobase + 'aqMIuUixqLQYmJITPnOGtFkRPuTqKa.png',
+    u"Русская ночь": logobase + '9Sh9bJuj6js5AJsypAd6UvwnsIB25R.png',
+    u"EuroNews": logobase + 'Vb3fP5gUK0q40WuzYeUhMT7RQmDg27.png',
+    u"Москва-24": logobase + 'dZcmoqRoZLhCBh8BE4RnbQivuDY6hH.png',
+    u"Россия 24": logobase + 'LWfGV6eICPYL7psaBfw2dOgGrOtHFS.png',
+    u"Cartoon network": logobase + 'NTNQLLri3Hh9iqYjW7VEkFYJsTLjk9.png',
+    u"Nick Jr": logobase + 'D87kJ3fIWIm5wKi5qxm24nbuPQv0U8.png',
+    u"В гостях у сказки": logobase + 'N076JCPKNaCl1T8ANnskFElL4t4uE8.png',
+    u"Канал Disney": logobase + 'JxEjTeXwExjnxutQGKJBmMI85tpNqK.png',
+    u"Канал О!": logobase + 'zHhfMQhsdu59e6puujg0qHt1675jYO.png',
+    u"Карусель": logobase + 'S233D4b6eq7KOXfdyi4dY2GokKeltg.png',
+#    u"Тлум HD": logobase + '',
+    u"Amedia 1": logobase + 'NbdxfVLZtRUQzzfvQPNRVMVAl2Hp0O.png',
+    u'Amedia Hit HD': logobase + 'HdnTfcZCgP7Odm1cOKNq9j4yJDRiFP.png',
+    u"Amedia Premium": logobase + 'ornzQpk6WCW6xk0lyBhlwqH8u2QyU7.png',
+    u"Cinema": logobase + 'J10aNphR3vEssL7rMO8Rtx6DLpDJfL.png',
+    u"Fox Life Россия": logobase + 'rkksGUl3DstQSEyT26Q07hNCEwyNnd.png',
+    u"Fox Россия": logobase + 'XGC77wQNeEyaJ2z2mDipyIPsoF0xc1.png',
+    u"Paramount Comedy Россия": logobase + '5EtvAWXB7VK1Yw82yvO28sY28dU4ZC.png',
+    u"Paramount Сhannel HD": logobase + 'Iyzjhb5BBRKfvEZdBwNJ785jzroODU.png',
+    u"Sony Channel": logobase + 'tKzOKIcOQYrFl1VL8B0QqERFXCAYfU.png',
+    u"TV1000 Action East": logobase + 'GblbxkDGXZyW5oWt9W8wuERQAiZ7ZT.png',
+    u"TV1000 East": logobase + 'WJMEvVafVakrm7BUMy1lzku7VQCx25.png',
+    u"TV1000 Русское кино": logobase + 'ch5DX6f8hxDnmyzrjotUoKHNGzcw9P.png',
+    u'ViP Megahit HD': logobase + 'mPzm416HTmW7uEPgjICGeKK95Tma5Y.png',
+    u"ViP Comedy HD": logobase + 'lO9PoF8cTaTlehJPMkkOqZM1cYEPcc.png',
+    u"ViP Premiere HD": logobase + 'NxpfVz8kXfNmwuj0cIhIQewuII9j7m.png',
+    u"Дом кино Премиум HD": logobase + 'Xg5l3gO5SnER0pwHhgsbHVMqSb42ty.png',
+    u"КиноПремиум HD": logobase + 'p580CRZ8bBS6dw3plMWhhxXSzQ59uS.png',
+    u"КиноТВ HD": logobase + 'NGQLEmlltpslVNKkD0htUF7CogGXqX.png',
+    u"Киномикс": logobase + 'y6b876ih8g7R876hfug3897wrhj.png',
+    u'Кинопоказ': logobase + 'L2qsABeMEDKOSG4Tvh7XpfvX2BfksG.png',
+    u"Кинопоказ HD1": logobase + 'pNA1vR3sPoovYNKzO4TU6NQcSXNqjk.png',
+    u"Кинопоказ HD2": logobase + 'Yf2XKrtorOF2wSZ23Q9NHhL2MfOcqi.png',
+    u"Киносвидание": logobase + 'y1876iuyf7645urjg56utfy.png',
+    u"Киносемья": logobase + 'y2oerfuhdj2108nbs4ev875esk47dn.png',
+    u'Кинохит': logobase + 'y3igbjZ&LGyqDWGIASUY78AWID.png',
+    u'Остросюжетное HD': logobase + 'mxF7CZsqsDRMMK4pN8ekdccEgvEsZC.png',
+    u'Страшное HD': logobase + 'cq6IE7NWJgUDkFFMKp65BbHTOniMje.png',
+    u'Родное кино': logobase + 'y7saqicb538iqo64ho46hh46.png',
+    u"НСТ (Страшное)": logobase + 'fKYzdlWRz68qd9mRZnWuxMY73EyaSz.png',
+    u"Наш Детектив HD": logobase + 'Dj0hDhHdckvGp86dDi058HRBe6JLq0.png',
+    u"Наше новое кино": logobase + 'y5jtghJCHG65ukyfjv 45stjxc76.png',
+    u"Русский детектив": logobase + '7I7VjbsFMIkZdoSbHFXiKEVZKNUbOM.png',
+    u"Русский иллюзион": logobase + 'E9Imfr8aHN5midPVpNhJ3fo49FHbQE.png',
+    u"ТВ XXI": logobase + 'TKchoTWZFRMmGDBok08zoEFJ8mJJCe.png',
+    u'Морской': logobase + 'uzlb3awoyNqvIcf6i35hTVqf7gvuqz.png',
+    u"Bridge HD": logobase + 'P7JEo7cGv1dtk6X69SkfEQeiVrQvXj.png',
+#    u"M1": logobase + '',
+    u"VH1 Classic": logobase + 'FhxUFQ2Bsfom4vb8Ce41gFObAbh1Vh.png',
+    u"VH1 Europe": logobase + '58.png',
+    u'MTV Dance': logobase + 'ZXKNRw6Ai8u4lY0wxeycQwj2dIkm66.png',
+    u"Eurosport 2 HD": logobase + '0mo5WOW3xn7FmcIRM9PMZxb2Zgad86.png',
+    u"Eurosport HD": logobase + 'DpFTzUEA3y67Z6ObTPF4xH0XLNRAZm.png',
+    u"Viasat Sport HD Россия": logobase + 'prAZKkny3W1HGM03lP0EhzcMmTPZdi.png',
+    u"Матч ТВ HD": logobase + 'MXyy9Uud7oDuH8JqVisjsD0csgAHnQ.png',
+    u"Матч! Наш спорт": logobase + 'l0x8GopxrHL6jLiDGVHi8tIEc7RBt2.png',
+    u"НТВ+ Наш футбол HD": logobase + '',
+    u"Сетанта Спорт + HD": logobase + 'X8VE9n2LhKfE67miEWg9VeioeA3QwT.png',
+    u'Че': logobase + 'Hv36ZG48lg1mm2wdxAo3ju1EFS41Ga.png',
+    u'Ю': logobase + 'YvnG7hXCwMmHnakp2KkCbqeCigHcuK.png',
+}
