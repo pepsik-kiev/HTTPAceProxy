03f696f59e5057adcd780ac2a26cecdf026ede80
Kleofass <spam@kleofass.lv>
2021-01-20 15:27:28 +0200

    ** Hotfix for check compatibility
    check_compatibility(gevent_version, psutil_version)

--- acehttp.py
+++ acehttp.py
@@ -441,15 +441,11 @@ def check_compatibility(gevent_version, psutil_version):
        major, minor, patch = map(int, gevent_version.split('.')[:3])
        # gevent >= 1.3.3
        assert major >= 1
-       assert minor >= 3
-       assert minor >= 3
 
     # Check psutil for compatibility.
     major, minor, patch = map(int, psutil_version.split('.')[:3])
     # psutil >= 5.3.0
-    assert major == 5
-    assert minor >= 3
-    assert patch >= 0
+    assert major >= 5
 
 logging.basicConfig(level=AceConfig.loglevel, filename=AceConfig.logfile, format=AceConfig.logfmt, datefmt=AceConfig.logdatefmt)
 logger = logging.getLogger('HTTPServer')
